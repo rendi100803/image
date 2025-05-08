2a4e05b394d89798f363d9dea63fec91aa0e89d8
rendi100803 <rendih100803@gmail.com>
2025-05-08 11:07:43 +0700

    setup docker build workflow

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__) 
 @app.route('/') 
 def home(): 
-	return "Hello from Container!" 
+	return "Hello from Container v2" 
 if __name__ == '__main__': 
 	app.run(host='0.0.0.0', port=5000)
